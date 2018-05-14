cbb6113f43077ddc397268ad999314ec70ec4836
youngthur <youngthur@naver.com>
2018-05-14 10:41:28 +0000

    i add two files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'gitest/index'
+
+  get 'gitest/create'
+
+  get 'gitest/update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
